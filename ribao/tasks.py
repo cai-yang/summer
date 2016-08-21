fd868865000cdc13368a34e0599a3bcbcab221a4
cai-yang <yc631207248@live.com>
2016-08-21 21:57:51 +0800

    change the tasks logic

--- ribao/tasks.py
+++ ribao/tasks.py
@@ -21,24 +21,26 @@ def check(daily_index):
     try:
         d=Daily.objects.get(pk=int(daily_index))
     except:
+        check.apply_async((daily_index + 1,), countdown=3)
         return "NO DAILY"
     article_list = Article.objects.filter(daily = d)
     try:
         p=article_list[4].title
     except:
+        check.apply_async((daily_index,), countdown=3)
         return "NOT FULL"
     flag = daily_index
     for article in article_list:
         if article.status=="0":
+            check.apply_async((daily_index,), countdown=3)
             return "NOT READY"
-            break
     chain = publish_daily.s(daily_index)|check.s()
     chain()
     return "SENDING"
 
 @shared_task
 def publish_daily(daily_index):
-    if email_daily(daily_index):
+    if email_daily.apply_async((daily_index,), countdown=3):
         d = Daily.objects.get(pk=daily_index)
         d.status="2"
         d.save()
@@ -48,7 +50,7 @@ def publish_daily(daily_index):
 
 @shared_task
 def email_daily(daily_index):
-    return "Daily No.%s has been e-mailed." % daily_index
+    return True
 
 @shared_task
 def add(x, y):
